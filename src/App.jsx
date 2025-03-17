642604549b6090f3680fb5dca5b48b4286ea2b03
pmmonisha31 <pmmonisha31@gmail.com>
2025-03-17 21:16:02 +0530

    Dashborad

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
+
 import Login from "./pages/Login";
 
 function App() {
