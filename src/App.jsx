1faed66216baaf4d02285b6acf9af532958bfc40
HGakash <hgakashgowda1234@gmail.com>
2025-03-17 21:10:47 +0530

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 
 function App() {
@@ -9,8 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-
+        
       </Routes>
     </Router>
   );
